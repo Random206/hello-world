8aefe528c1f660ea0a200cb1b5ca9ea78bc55af8
Toby <61461732+Random206@users.noreply.github.com>
2022-02-17 22:36:58 +1000

    Revert "Merge pull request #2 from Random206/feature/login"
    This reverts commit 839809bb60eb79236ec126bd5ba5f95d744eb362, reversing
    changes made to 07451f4e0820004af058ba784e992ca73b57a8b4.

--- Hello-World/ViewController.swift
+++ Hello-World/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Start to work on login feature
+        // Do any additional setup after loading the view.
     }
 
 
